0ed5a0f1a0f38123c528ae080447ad08659b03d7
김하빈 <runk7gkqls@gmail.com>
2026-01-22 15:48:11 +0900

    Add SkillDataAsset

---
+++ Source/ProjectER/SkillSystem/SkillDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SkillDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API USkillDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
