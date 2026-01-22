c7b9399f9cf76ce7c440151dafee87c71ac15eb8
khee8469 <bcw1992@naver.com>
2026-01-22 16:53:19 +0900

    Add : BaseMonster, AttributeSet

---
+++ Source/ProjectER/Monster/BaseMonster.cpp
@@ -0,0 +1,56 @@
+﻿#include "Monster/BaseMonster.h"
+
+#include "AbilitySystemComponent.h"
+#include "AttributeSet/BaseMonsterAttributeSet.h"
+
+ABaseMonster::ABaseMonster()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	ASC->SetIsReplicated(true);
+	ASC->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+	AttributeSet = CreateDefaultSubobject<UBaseMonsterAttributeSet>(TEXT("AttributeSet"));
+
+}
+
+void ABaseMonster::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	InitAbilitySystem();
+}
+
+void ABaseMonster::PossessedBy(AController* newController)
+{
+	UE_LOG(LogTemp, Warning, TEXT("AI Controller Possessed"));
+}
+
+UAbilitySystemComponent* ABaseMonster::GetAbilitySystemComponent() const
+{
+	return ASC;
+}
+
+void ABaseMonster::InitAbilitySystem()
+{
+	if (!ASC)
+	{
+		return;
+	}
+
+	ASC->InitAbilityActorInfo(this, this);
+	InitGiveAbilities();
+}
+
+void ABaseMonster::InitGiveAbilities()
+{
+	//if (!HasAuthority()) { return; }
+	
+	for (TSubclassOf<UGameplayAbility> Ability : DefaultAbilities)
+	{
+		ASC->GiveAbility(
+			FGameplayAbilitySpec(Ability, 1, INDEX_NONE, this)
+		);
+	}
+}
---
+++ Source/ProjectER/Monster/BaseMonster.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+//#include "Abilities/GameplayAbility.h"
+#include "BaseMonster.generated.h"
+
+class UBaseMonsterAttributeSet;
+class UGameplayAbility;
+class UStateTreeComponent;
+
+UCLASS()
+class PROJECTER_API ABaseMonster : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ABaseMonster();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void PossessedBy(AController* newController) override;
+
+private:
+	void InitAbilitySystem();
+
+	void InitGiveAbilities();
+
+public:
+
+
+protected:
+
+
+private:
+
+#pragma region GAS
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAbilitySystemComponent> ASC;
+
+	UPROPERTY()
+	TObjectPtr<UBaseMonsterAttributeSet> AttributeSet;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = "true"))
+	TArray<TSubclassOf<UGameplayAbility>> DefaultAbilities;
+
+
+#pragma endregion
+
+};
+
+
+
