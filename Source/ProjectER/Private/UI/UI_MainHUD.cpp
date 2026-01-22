8a386b57b6d97f8943def296983123f1f7b0458e
CranberryIsGoodTaste <104151171+CranberryMan@users.noreply.github.com>
2026-01-22 19:50:27 +0900

    Add Main HUD Add TestLevel Update Main HUD  * 기본 레이아웃 생성  * 프로그레스바 동작  * 스킬 레벨업 버튼 show/hide 처리

--- Source/ProjectER/Private/UI/UI_MainHUD.cpp
+++ Source/ProjectER/Private/UI/UI_MainHUD.cpp
@@ -1,5 +1,55 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "UI/UI_MainHUD.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Components/ProgressBar.h"
 
+void UUI_MainHUD::Update_HP(float CurrentHP, float MaxHP)
+{
+    if (IsValid(PB_HP))
+    {
+        float HealthPercent = CurrentHP / MaxHP;
+        PB_HP->SetPercent(HealthPercent);
+    }
+
+    if (IsValid(current_HP))
+    {
+		current_HP->SetText(FText::AsNumber(FMath::RoundToInt(CurrentHP)));
+    }
+}
+
+void UUI_MainHUD::UPdate_MP(float CurrentMP, float MaxMP)
+{
+    if (IsValid(PB_MP))
+    {
+        float HealthPercent = CurrentMP / MaxMP;
+        PB_MP->SetPercent(HealthPercent);
+    }
+
+    if (IsValid(current_MP))
+    {
+        current_MP->SetText(FText::AsNumber(FMath::RoundToInt(CurrentMP)));
+    }
+}
+
+void UUI_MainHUD::ShowSkillUp(bool show)
+{
+    if (IsValid(skill_up_01))
+    {
+        skill_up_01->SetVisibility(show ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+    }
+    if (IsValid(skill_up_02))
+    {
+        skill_up_02->SetVisibility(show ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+    }
+    if (IsValid(skill_up_03))
+    {
+        skill_up_03->SetVisibility(show ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+    }
+    if (IsValid(skill_up_04))
+    {
+        skill_up_04->SetVisibility(show ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+	}
+}
--- Source/ProjectER/Public/UI/UI_MainHUD.h
+++ Source/ProjectER/Public/UI/UI_MainHUD.h
@@ -1,10 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 #include "UI_MainHUD.generated.h"
+
+class UTextBlock;
+class UButton;
+class UProgressBar;
 
 /**
  * 
@@ -14,4 +19,73 @@ class PROJECTER_API UUI_MainHUD : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable, Category = "UI_MainHUD")
+	void Update_HP(float CurrentHP, float MaxHP);
+	UFUNCTION(BlueprintCallable, Category = "UI_MainHUD")
+	void UPdate_MP(float CurrentHP, float MaxHP);
+	UFUNCTION(BlueprintCallable, Category = "UI_MainHUD")
+	void ShowSkillUp(bool show);
+
+protected:
+	///  stat_nn = 임시명칭
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* stat_01;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* stat_02;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* stat_03;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* stat_04;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* stat_05;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* stat_06;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_01;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_02;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_03;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_04;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_up_01;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_up_02;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_up_03;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* skill_up_04;
+
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* PB_HP;
+
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* PB_MP;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* current_HP;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* max_HP;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* current_MP;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* max_MP;
 };
