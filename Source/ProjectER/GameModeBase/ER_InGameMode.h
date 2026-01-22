df30ff3c0802fc94183c2d789905ad7e975a8e46
minsungJun <asrjatk@gmail.com>
2026-01-22 16:29:12 +0900

    Add: GameMode
    아웃게임 인게임 게임모드 추가

---
+++ Source/ProjectER/GameModeBase/ER_InGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ER_InGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API AER_InGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectER/GameModeBase/ER_OutGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ER_OutGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API AER_OutGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
