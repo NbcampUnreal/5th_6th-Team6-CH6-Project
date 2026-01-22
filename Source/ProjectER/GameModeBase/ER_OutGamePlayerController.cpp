34db72609fd181114aab7c419d4bc6a641a1777b
minsungJun <asrjatk@gmail.com>
2026-01-22 18:06:10 +0900

    update: ER_OutGamePlayerController
    RPC 추가

--- Source/ProjectER/GameModeBase/ER_OutGamePlayerController.cpp
+++ Source/ProjectER/GameModeBase/ER_OutGamePlayerController.cpp
@@ -1,5 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "GameModeBase/ER_OutGamePlayerController.h"
+#include "ER_OutGamePlayerController.h"
+#include "ER_OutGameMode.h"
 
+void AER_OutGamePlayerController::ConnectToDedicatedServer(const FString& Ip, int32 Port)
+{
+	if (!IsLocalController())
+		return;
+
+	const FString Address = FString::Printf(TEXT("%s:%d"), *Ip, Port);
+
+	UE_LOG(LogTemp, Log, TEXT("[PC] Connecting to server: %s"), *Address);
+
+	ClientTravel(Address, TRAVEL_Absolute);
+}
+
+
+void AER_OutGamePlayerController::Server_StartGame_Implementation()
+{
+	auto OutGameMode = Cast<AER_OutGameMode>(GetWorld()->GetAuthGameMode());
+	OutGameMode->StartGame();
+}
+
+void AER_OutGamePlayerController::Server_TEMP_OutGame_Implementation()
+{
+	auto OutGameMode = Cast<AER_OutGameMode>(GetWorld()->GetAuthGameMode());
+	OutGameMode->EndGame();
+}
--- Source/ProjectER/GameModeBase/ER_OutGamePlayerController.h
+++ Source/ProjectER/GameModeBase/ER_OutGamePlayerController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,12 +6,21 @@
 #include "GameFramework/PlayerController.h"
 #include "ER_OutGamePlayerController.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class PROJECTER_API AER_OutGamePlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Network")
+	void ConnectToDedicatedServer(const FString& Ip, int32 Port);
+
+private:
+	UFUNCTION(BlueprintCallable, Server, Reliable)
+	void Server_StartGame();
+
+	UFUNCTION(BlueprintCallable, Server, Reliable)
+	void Server_TEMP_OutGame();
+
 };
