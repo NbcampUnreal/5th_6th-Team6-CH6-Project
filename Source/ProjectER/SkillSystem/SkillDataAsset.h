2d2b65248ef01f24b8219672a08f43b1b9a16dc5
김하빈 <runk7gkqls@gmail.com>
2026-01-22 20:51:45 +0900

    Update SkillDataAsset.h

--- Source/ProjectER/SkillSystem/SkillDataAsset.h
+++ Source/ProjectER/SkillSystem/SkillDataAsset.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "GameplayTagContainer.h"
+#include "GameplayEffect.h"
 #include "SkillDataAsset.generated.h"
 
 /**
@@ -20,26 +21,35 @@ enum class ESkillActivationType : uint8 {
     Holding    UMETA(DisplayName = "Holding")
 };
 
-UENUM(BlueprintType)
-enum class ESkillFormType : uint8 {
-    Fixed      UMETA(DisplayName = "고정 위치(소환)"),
-    Projectile UMETA(DisplayName = "투사체(직선)"),
-    Homing     UMETA(DisplayName = "추적(호밍)"),
-    Attached   UMETA(DisplayName = "부착형")
-};
+//UENUM(BlueprintType)
+//enum class ESkillFormType : uint8 {
+//    Fixed      UMETA(DisplayName = "고정 위치(소환)"),
+//    Projectile UMETA(DisplayName = "투사체(직선)"),
+//    Homing     UMETA(DisplayName = "추적(호밍)"),
+//    Attached   UMETA(DisplayName = "부착형")
+//};
 
 USTRUCT(BlueprintType)
-struct FSkillModifierInfo {
+struct FSkillInfo {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere)
+    UPROPERTY(EditDefaultsOnly, Category = "Effect")
+    TSubclassOf<UGameplayEffect> SkillEffectClass;
+
+    UPROPERTY(EditDefaultsOnly)
     FGameplayTag StatTag; // 참조할 스탯 (예: Stat.AttackPower, Stat.AbilityPower)
 
-    UPROPERTY(EditAnywhere)
+    UPROPERTY(EditDefaultsOnly)
     float Coefficient;    // 계수 (예: 0.6, 1.2)
 
     UPROPERTY(EditAnywhere)
+    float CoeffPerLevel;
+
+    UPROPERTY(EditDefaultsOnly)
     float BaseDamage;     // 스킬 기본 데미지
+
+    UPROPERTY(EditDefaultsOnly)
+    float ValuePerLevel; // 레벨별 스킬 기본 데미지 증가량
 };
 
 UCLASS()
@@ -48,9 +58,9 @@ class PROJECTER_API USkillDataAsset : public UDataAsset
 	GENERATED_BODY()
 	
 public:
-
-
+    UPROPERTY(EditDefaultsOnly, Category = "Skill|Damage")
+    ESkillActivationType SkillActivationType;
 
     UPROPERTY(EditDefaultsOnly, Category = "Skill|Damage")
-    TArray<FSkillModifierInfo> DamageModifiers;
+    TArray<FSkillInfo> DamageInfos;
 };
