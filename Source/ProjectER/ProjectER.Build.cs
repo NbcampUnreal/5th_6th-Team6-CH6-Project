cbd391462a7227efaebab2e2f942a82869fa8b4b
minsungJun <asrjatk@gmail.com>
2026-01-22 16:05:21 +0900

    Add: Files
    WBP, GameMode, Controller 추가

--- Source/ProjectER/ProjectER.Build.cs
+++ Source/ProjectER/ProjectER.Build.cs
@@ -30,12 +30,16 @@ public class ProjectER : ModuleRules
 
         PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicIncludePaths.AddRange(new string[] {
+            "ProjectER"
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
