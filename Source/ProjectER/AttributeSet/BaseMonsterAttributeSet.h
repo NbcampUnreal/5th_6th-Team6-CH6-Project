ad8ab50c9ecf66996d89e0883dae606c4cb2956f
김하빈 <runk7gkqls@gmail.com>
2026-01-23 20:55:33 +0900

    Move Files

--- Source/ProjectER/AttributeSet/BaseMonsterAttributeSet.h
+++ Source/ProjectER/AttributeSet/BaseMonsterAttributeSet.h
@@ -3,15 +3,15 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
-
+#include "CharacterSystem/GAS/AttributeSet/BaseAttributeSet.h"
 #include "BaseMonsterAttributeSet.generated.h"
 
 
-#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)\
-GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName)\
-GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)\
-GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)\
-GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+//#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)\
+//GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName)\
+//GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)\
+//GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)\
+//GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 
 UCLASS()
