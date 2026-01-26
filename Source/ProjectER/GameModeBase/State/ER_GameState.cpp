a8f8b8ce741ade4fbc4266b953f4d84828a7935d
minsungJun <asrjatk@gmail.com>
2026-01-26 16:13:16 +0900

    Update: Caching PlayerArray
    팀 별로 캐싱

--- Source/ProjectER/GameModeBase/State/ER_GameState.cpp
+++ Source/ProjectER/GameModeBase/State/ER_GameState.cpp
@@ -1,2 +1,29 @@
 ﻿#include "ER_GameState.h"
+#include "GameModeBase/State/ER_PlayerState.h"
 
+void AER_GameState::BuildTeamCache()
+{
+	if (!HasAuthority()) 
+		return;
+
+	for (APlayerState* PS : PlayerArray)
+	{
+		if (AER_PlayerState* ERPS = Cast<AER_PlayerState>(PS))
+		{
+			const int32 TeamIdx = static_cast<int32>(ERPS->Team);
+			TeamCache[TeamIdx].AddUnique(ERPS);
+		}
+	}
+
+}
+
+void AER_GameState::RemoveTeamCache()
+{
+	if (!HasAuthority()) 
+		return;
+
+	for (auto& Arr : TeamCache)
+	{
+		Arr.Reset();
+	}
+}
--- Source/ProjectER/GameModeBase/State/ER_GameState.h
+++ Source/ProjectER/GameModeBase/State/ER_GameState.h
@@ -4,12 +4,19 @@
 #include "GameFramework/GameStateBase.h"
 #include "ER_GameState.generated.h"
 
-/**
- * 
- */
+
+class AER_PlayerState;
+
 UCLASS()
 class PROJECTER_API AER_GameState : public AGameStateBase
 {
 	GENERATED_BODY()
 	
+public:
+	void BuildTeamCache();
+	void RemoveTeamCache();
+	
+
+public:
+	TArray<TArray<TWeakObjectPtr<AER_PlayerState>>> TeamCache;
 };
