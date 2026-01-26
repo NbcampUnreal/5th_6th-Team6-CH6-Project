e82d8e78bd3ca87c6b592e57f2bc7f11a390d3d9
minsungJun <asrjatk@gmail.com>
2026-01-26 20:21:53 +0900

    Update: ER_GameState
    팀 전멸 판정 map 추가 및 헬퍼 추가

--- Source/ProjectER/GameModeBase/State/ER_GameState.cpp
+++ Source/ProjectER/GameModeBase/State/ER_GameState.cpp
@@ -6,14 +6,31 @@ void AER_GameState::BuildTeamCache()
 	if (!HasAuthority()) 
 		return;
 
+	RemoveTeamCache();
+
+	const int32 TeamCount = (int32)ETeam::Team3;
+
+	for (int32 TeamIdx = 1; TeamIdx < TeamCount; ++TeamIdx)
+	{
+		TeamElimination.Add(TeamIdx, false);
+		UE_LOG(LogTemp, Log, TEXT("TeamElimination | %d"), TeamIdx);
+	}
+
 	for (APlayerState* PS : PlayerArray)
 	{
 		if (AER_PlayerState* ERPS = Cast<AER_PlayerState>(PS))
 		{
 			const int32 TeamIdx = static_cast<int32>(ERPS->Team);
+
+			if (!TeamCache.IsValidIndex(TeamIdx))
+				continue;
+
 			TeamCache[TeamIdx].AddUnique(ERPS);
+
+			TeamElimination.FindOrAdd(TeamIdx) = false;
 		}
 	}
+
 
 }
 
@@ -25,6 +42,30 @@ void AER_GameState::RemoveTeamCache()
 	for (auto& Team : TeamCache)
 	{
 		Team.Reset();
+	}
+
+	TeamElimination.Empty();
+}
+
+void AER_GameState::CheckTeamEliminate(int32 idx)
+{
+	int32 AliveCount = 0;
+
+	if (TeamCache.IsValidIndex(idx))
+	{
+		for (auto& WeakPS : TeamCache[idx])
+		{
+			AER_PlayerState* PS = WeakPS.Get();
+			if (PS && !PS->bIsDead)
+			{
+				++AliveCount;
+			}
+		}
+	}
+
+	if (AliveCount == 0)
+	{
+		TeamElimination[idx] = true;
 	}
 }
 
--- Source/ProjectER/GameModeBase/State/ER_GameState.h
+++ Source/ProjectER/GameModeBase/State/ER_GameState.h
@@ -15,7 +15,14 @@ class PROJECTER_API AER_GameState : public AGameStateBase
 public:
 	void BuildTeamCache();
 	void RemoveTeamCache();
+	void CheckTeamEliminate(int32 idx);
+
+	// Getter
+	bool GetTeamElimination(int32 idx) { return TeamElimination[idx]; }
 
 public:
 	TArray<TArray<TWeakObjectPtr<AER_PlayerState>>> TeamCache;
+
+	UPROPERTY(BlueprintReadOnly)
+	TMap<int32, bool> TeamElimination;
 };
