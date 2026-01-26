4ddd67d253f33d1e20db21a2d9ca8d40b18adb3e
CranberryIsGoodTaste <104151171+CranberryMan@users.noreply.github.com>
2026-01-26 19:17:20 +0900

    Add : title image

---
+++ Source/ProjectER/GameModeBase/ER_TitleGameMode.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModeBase/ER_TitleGameMode.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Blueprint/UserWidget.h"
+
+AER_TitleGameMode::AER_TitleGameMode()
+{
+}
+
+void AER_TitleGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // UI는 서버에선 실행 안되게
+    if (UKismetSystemLibrary::IsDedicatedServer(GetWorld()))
+    {
+        return;
+    }
+
+    APlayerController* PC = GetWorld()->GetFirstPlayerController();  
+    if (PC && PC->IsLocalController() && TitleWidgetClass)
+    {
+        UUserWidget* TitleUI = CreateWidget<UUserWidget>(GetWorld(), TitleWidgetClass);
+        if (TitleUI)
+        {
+            TitleUI->AddToViewport();
+
+            FInputModeUIOnly InputMode;
+            InputMode.SetWidgetToFocus(TitleUI->TakeWidget());
+            PC->SetInputMode(InputMode);
+            PC->bShowMouseCursor = true;
+        }
+    }
+}
---
+++ Source/ProjectER/GameModeBase/ER_TitleGameMode.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameModeBase/ER_GameModeBase.h"
+#include "ER_TitleGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API AER_TitleGameMode : public AER_GameModeBase
+{
+	GENERATED_BODY()
+public:
+    AER_TitleGameMode();
+
+protected:
+    virtual void BeginPlay() override;
+
+    // 타이틀 UI 블루프린트에서 집어넣는거 잊지 말기
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+    TSubclassOf<class UUserWidget> TitleWidgetClass;
+
+    UPROPERTY()
+    class UUserWidget* CurrentTitleWidget;
+};
