4d7dbb6565db3784b9fd69f7347c0f94787d7c76
minsungJun <asrjatk@gmail.com>
2026-01-27 21:10:30 +0900

    Update: PlayerController
    UI 출력 테스트용 함수 추가

--- Source/ProjectER/GameModeBase/ER_OutGamePlayerController.cpp
+++ Source/ProjectER/GameModeBase/ER_OutGamePlayerController.cpp
@@ -1,5 +1,59 @@
 ﻿#include "ER_OutGamePlayerController.h"
+#include "GameModeBase/State/ER_PlayerState.h"
 #include "GameMode/ER_OutGameMode.h"
+#include "Blueprint/UserWidget.h"
+
+// 인 게임 컨트롤러로 이식 필요
+void AER_OutGamePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AER_PlayerState* PS = GetPlayerState<AER_PlayerState>())
+	{
+		PS->OnWinChanged.AddUObject(this, &AER_OutGamePlayerController::HandleLoseChanged);
+		PS->OnWinChanged.AddUObject(this, &AER_OutGamePlayerController::HandleWinChanged);
+	}
+}
+
+// 인 게임 컨트롤러로 이식 필요
+void AER_OutGamePlayerController::HandleLoseChanged(bool bLose)
+{
+	if (!IsLocalController()) return;
+
+	if (bLose)
+	{
+		// 탈락 UI 띄우기, 입력 제한, 커서 ON
+		if (!LoseUIClass)
+			return;
+
+		UE_LOG(LogTemp, Log, TEXT("[PC] : HandleLoseChanged"));
+		LoseUIInstance = CreateWidget<UUserWidget>(this, LoseUIClass);
+	}
+	else
+	{
+		
+	}
+}
+
+// 인 게임 컨트롤러로 이식 필요
+void AER_OutGamePlayerController::HandleWinChanged(bool bWin)
+{
+	if (!IsLocalController()) return;
+
+	if (bWin)
+	{
+		// 승리 UI 띄우기, 입력 제한, 커서 ON
+		if (!WinUIClass)
+			return;
+
+		UE_LOG(LogTemp, Log, TEXT("[PC] : HandleWinChanged"));
+		WinUIInstance = CreateWidget<UUserWidget>(this, WinUIClass);
+	}
+	else
+	{
+		
+	}
+}
 
 void AER_OutGamePlayerController::ConnectToDedicatedServer(const FString& Ip, int32 Port, const FString& PlayerName)
 {
@@ -12,6 +66,8 @@ void AER_OutGamePlayerController::ConnectToDedicatedServer(const FString& Ip, in
 
 	ClientTravel(Address, TRAVEL_Absolute);
 }
+
+
 
 
 void AER_OutGamePlayerController::Server_StartGame_Implementation()
--- Source/ProjectER/GameModeBase/ER_OutGamePlayerController.h
+++ Source/ProjectER/GameModeBase/ER_OutGamePlayerController.h
@@ -10,6 +10,10 @@ class PROJECTER_API AER_OutGamePlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+private:
+	virtual void BeginPlay() override;
+
+
 public:
 	UFUNCTION(BlueprintCallable, Category = "Network")
 	void ConnectToDedicatedServer(const FString& Ip, int32 Port, const FString& PlayerName);
@@ -20,4 +24,23 @@ private:
 
 	UFUNCTION(BlueprintCallable, Server, Reliable)
 	void Server_TEMP_OutGame();
+
+
+	// 인 게임 컨트롤러로 이식 필요
+	void HandleLoseChanged(bool bLose);
+	void HandleWinChanged(bool bWin);
+
+
+	// UI 출력 테스트
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> LoseUIClass;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UUserWidget> LoseUIInstance;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> WinUIClass;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UUserWidget> WinUIInstance;
 };
