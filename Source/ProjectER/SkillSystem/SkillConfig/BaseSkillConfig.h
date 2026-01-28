2b217d6595b5a9ac881807764764f1d3bdc9ea9e
김하빈 <runk7gkqls@gmail.com>
2026-01-28 10:38:12 +0900

    Add UBaseSkillConfig

---
+++ Source/ProjectER/SkillSystem/SkillConfig/BaseSkillConfig.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "BaseSkillConfig.generated.h"
+
+/**
+ * 
+ */
+
+class USkillBase;
+
+UCLASS()
+class PROJECTER_API UBaseSkillConfig : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Default|Ability")
+	TSoftClassPtr<USkillBase> Ability;
+
+};
