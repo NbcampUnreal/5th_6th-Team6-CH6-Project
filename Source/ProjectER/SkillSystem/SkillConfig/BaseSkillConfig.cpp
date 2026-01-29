7774ae2ed0a724d47c3c7c75e149419944592d6c
김하빈 <runk7gkqls@gmail.com>
2026-01-29 11:30:10 +0900

    Updata UBaseSkillConfig

--- Source/ProjectER/SkillSystem/SkillConfig/BaseSkillConfig.cpp
+++ Source/ProjectER/SkillSystem/SkillConfig/BaseSkillConfig.cpp
@@ -1,5 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "SkillSystem/SkillConfig/BaseSkillConfig.h"
+#include "Skillsystem/GameAbility/SkillBase.h"
+#include "SkillSystem/GameAbility/MouseTargetSkill.h"
 
+UBaseSkillConfig::UBaseSkillConfig()
+{
+	AbilityClass = USkillBase::StaticClass();
+}
+
+UMouseTargetSkillConfig::UMouseTargetSkillConfig()
+{
+	AbilityClass = UMouseTargetSkill::StaticClass();
+	SkillData.SkillActivationType = ESkillActivationType::Targeted;
+}
--- Source/ProjectER/SkillSystem/SkillConfig/BaseSkillConfig.h
+++ Source/ProjectER/SkillSystem/SkillConfig/BaseSkillConfig.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
+#include "SkillSystem/SkillData.h"
 #include "BaseSkillConfig.generated.h"
 
 /**
@@ -12,15 +13,38 @@
 
 class USkillBase;
 
-UCLASS()
+UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
 class PROJECTER_API UBaseSkillConfig : public UObject
 {
 	GENERATED_BODY()
 	
 public:
+	UBaseSkillConfig();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Data")
+	FSkillDefaultData SkillData;
 
 protected:
-	UPROPERTY(EditDefaultsOnly, Category = "Default|Ability")
-	TSoftClassPtr<USkillBase> Ability;
+	UPROPERTY(VisibleAnywhere, Category = "AbilityType", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<USkillBase> AbilityClass;
 
+	/*UPROPERTY(EditDefaultsOnly, Category = "Skill|Effect")
+	TArray<FSkillEffectData> EffectDatas;*/
+};
+
+UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
+class PROJECTER_API UMouseTargetSkillConfig : public UBaseSkillConfig
+{
+	GENERATED_BODY()
+
+public:
+	UMouseTargetSkillConfig();
+	FORCEINLINE float GetRange() const { return Range; }
+	FORCEINLINE const TArray<FSkillEffectData>& GetEffects() const { return EffectsToApply; }
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "AbilityType", meta = (AllowPrivateAccess = "true"))
+	float Range;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Skill|Effect")
+	TArray<FSkillEffectData> EffectsToApply;
 };
