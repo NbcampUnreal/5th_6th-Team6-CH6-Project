09d2c32f0bf5ad02894db8ca3f8788c945aa938e
minsungJun <asrjatk@gmail.com>
2026-01-30 17:04:22 +0900

    Update: ER_PlayerState
    BasePlayerState 이식

--- Source/ProjectER/GameModeBase/State/ER_PlayerState.cpp
+++ Source/ProjectER/GameModeBase/State/ER_PlayerState.cpp
@@ -1,9 +1,21 @@
 ﻿#include "ER_PlayerState.h"
 #include "Net/UnrealNetwork.h"
+#include "AbilitySystemComponent.h"
+#include "CharacterSystem/GAS/AttributeSet/BaseAttributeSet.h"
 
 AER_PlayerState::AER_PlayerState()
 {
 	bReplicates = true;
+
+	// ASC 생성 및 설정
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true); // ASC 상태 복제
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed); // ReplicationMode 설정
+
+	// Attribute Set 생성
+	AttributeSet = CreateDefaultSubobject<UBaseAttributeSet>("AttributeSet"); // Attribute Set 생성
+
+	SetNetUpdateFrequency(100.0f); // 네트워크(갱신 주기) 최적화
 }
 
 void AER_PlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -33,12 +45,8 @@ void AER_PlayerState::CopyProperties(APlayerState* PlayerState)
 	}
 }
 
-void AER_PlayerState::OnRep_IsLose()
+UAbilitySystemComponent* AER_PlayerState::GetAbilitySystemComponent() const
 {
-	OnLoseChanged.Broadcast(bIsLose);
+	return AbilitySystemComponent;
 }
 
-void AER_PlayerState::OnRep_IsWin()
-{
-	OnWinChanged.Broadcast(bIsWin);
-}
