2aa964c0d8fbd488857a2441a3937020b707e566
김하빈 <runk7gkqls@gmail.com>
2026-02-10 20:33:49 +0900

    Add UBaseExecutionCalculation

---
+++ Source/ProjectER/SkillSystem/GameplayEffectExecutionCalculation/BaseExecutionCalculation.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "BaseExecutionCalculation.generated.h"
+
+/**
+ * 
+ */
+struct FSkillEffectDefinition;
+UCLASS()
+class PROJECTER_API UBaseExecutionCalculation : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+
+public:
+	UBaseExecutionCalculation();
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+
+protected:
+	float ReturnCalculatedValue(const FGameplayEffectCustomExecutionParameters& ExecutionParams, const FSkillEffectDefinition& SkillEffectDefinition, const float Level) const;
+private:
+};
