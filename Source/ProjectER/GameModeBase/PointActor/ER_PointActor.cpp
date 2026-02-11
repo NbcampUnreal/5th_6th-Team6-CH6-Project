d127a7f375dfec46b5a035a9bacc6363d4573d9e
minsungJun <asrjatk@gmail.com>
2026-02-11 19:35:02 +0900

    Add: ER_PointActor
    몹 스폰, 플레이어 리스폰, 오브젝트 스폰의 위치를 지정할 cpp파일 추가

---
+++ Source/ProjectER/GameModeBase/PointActor/ER_PointActor.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModeBase/PointActor/ER_PointActor.h"
+#include "GameModeBase/Subsystem/Respawn/ER_RespawnSubsystem.h"
+
+// Sets default values
+AER_PointActor::AER_PointActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AER_PointActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AER_PointActor::EndPlay(const EEndPlayReason::Type Reason)
+{
+
+	Super::EndPlay(Reason);
+}
+
+// Called every frame
+void AER_PointActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ProjectER/GameModeBase/PointActor/ER_PointActor.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ER_PointActor.generated.h"
+
+UENUM(BlueprintType)
+enum class EPointActorType : uint8
+{
+	None,
+	SpawnPoint,
+	RespawnPoint,
+	ObjectPoint,
+};
+
+
+UCLASS()
+class PROJECTER_API AER_PointActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AER_PointActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type Reason) override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite)
+	EPointActorType PointType = EPointActorType::None;
+
+};
