8cd687ec71d21bcfa0d51ec35a8596c9f590199c
minsungJun <asrjatk@gmail.com>
2026-02-11 19:36:32 +0900

    Add: ER_ObjectSubssystem
    오브젝트의 관리를 맡을 subsystem 추가

---
+++ Source/ProjectER/GameModeBase/Subsystem/Object/ER_ObjectSubsystem.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "ER_ObjectSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API UER_ObjectSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
