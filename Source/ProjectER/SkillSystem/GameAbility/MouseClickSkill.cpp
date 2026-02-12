b7ba73435052ae31bc01fb3201bbd6ea3192a1d5
김하빈 <runk7gkqls@gmail.com>
2026-02-12 20:27:19 +0900

    Add UMouseClickSkill

---
+++ Source/ProjectER/SkillSystem/GameAbility/MouseClickSkill.cpp
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SkillSystem/GameAbility/MouseClickSkill.h"
+#include "Abilities/Tasks/AbilityTask_WaitTargetData.h"
+#include "AbilitySystemComponent.h"
+
+void UMouseClickSkill::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	PrepareDataSetDelegate(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (IsLocallyControlled())
+	{
+		SendLocationData(GetMouseLocation());
+	}
+	else {
+		//DoNoting
+	}
+}
+
+void UMouseClickSkill::SendLocationData(FVector TargetLocation)
+{
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	if (!ASC) return;
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_LocationInfo* LocData = new FGameplayAbilityTargetData_LocationInfo();
+	LocData->TargetLocation.LiteralTransform = FTransform(TargetLocation);
+	DataHandle.Add(LocData);
+
+	FScopedPredictionWindow ScopedPrediction(ASC, CurrentActorInfo->IsLocallyControlled());
+	ASC->CallServerSetReplicatedTargetData(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey(), DataHandle, FGameplayTag(), ASC->ScopedPredictionKey);
+	//ASC->CallReplicatedTargetDataDelegatesIfSet(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey());
+}
+
+bool UMouseClickSkill::IsInRange(const FVector& Location)
+{
+	return false;
+}
+
+void UMouseClickSkill::RotateToLocation(const FVector& Location)
+{
+}
+
+void UMouseClickSkill::OnTargetDataReady(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag)
+{
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	check(ASC);
+	if (!CommitAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo)) return;
+
+	FVector FinalLocation = DataHandle.Get(0)->GetEndPoint();
+
+	// 장판 소환!!
+	//SpawnSummonArea(FinalLocation);
+
+	// 마무리 작업
+	ASC->AbilityTargetDataSetDelegate(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey()).Remove(TargetDataDelegateHandle);
+	ASC->ConsumeClientReplicatedTargetData(CurrentSpecHandle, CurrentActivationInfo.GetActivationPredictionKey());
+}
+
+void UMouseClickSkill::ExecuteSkill()
+{
+
+}
+
+
+void UMouseClickSkill::PrepareDataSetDelegate(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	check(ASC);
+
+	TargetDataDelegateHandle = ASC->AbilityTargetDataSetDelegate(Handle, ActivationInfo.GetActivationPredictionKey()).AddUObject(this, &UMouseClickSkill::OnTargetDataReady);
+	ASC->CallReplicatedTargetDataDelegatesIfSet(Handle, ActivationInfo.GetActivationPredictionKey());
+}
+
+FVector UMouseClickSkill::GetMouseLocation() const
+{
+	APlayerController* PC = GetActorInfo().PlayerController.Get();
+	if (PC)
+	{
+		FHitResult Hit;
+		if (PC->GetHitResultUnderCursor(ECC_Visibility, false, Hit))
+		{
+			return Hit.Location;
+		}
+	}
+	return FVector::ZeroVector;
+}
---
+++ Source/ProjectER/SkillSystem/GameAbility/MouseClickSkill.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SkillSystem/GameAbility/SkillBase.h"
+#include "MouseClickSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API UMouseClickSkill : public USkillBase
+{
+	GENERATED_BODY()
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	void SendLocationData(FVector TargetLocation);
+protected:
+	virtual void ExecuteSkill() override;
+	void PrepareDataSetDelegate(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData);
+	bool IsInRange(const FVector& Location);
+	void RotateToLocation(const FVector& Location);
+	FVector GetMouseLocation() const;
+	UFUNCTION()
+	void OnTargetDataReady(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
+	//UFUNCTION()
+	//void OnTargetCancelled(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
+private:
+
+public:
+
+protected:
+	FDelegateHandle TargetDataDelegateHandle;
+private:
+};
