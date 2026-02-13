a5d40633be0f4568ab5166f07f5907f1b1ecb6bc
김하빈 <runk7gkqls@gmail.com>
2026-02-13 20:09:20 +0900

    Add BaseGEC

---
+++ Source/ProjectER/SkillSystem/GameplayEffectComponent/BaseGEC.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectComponent.h"
+#include "BaseGEC.generated.h"
+
+/**
+ * 
+ */
+
+class UBaseGECConfig;
+
+UCLASS()
+class PROJECTER_API UBaseGEC : public UGameplayEffectComponent
+{
+	GENERATED_BODY()
+
+public:
+	UBaseGEC();
+
+	FORCEINLINE UBaseGECConfig* GetConfig() const { return CachedConfig; }
+
+	virtual TSubclassOf<UBaseGECConfig> GetRequiredConfigClass() const;
+protected:
+	UPROPERTY()
+	TObjectPtr<UBaseGECConfig> CachedConfig;
+
+	UPROPERTY()
+	TSubclassOf<UBaseGECConfig> ConfigClass;
+private:
+
+public:
+
+protected:
+
+private:
+	
+};
