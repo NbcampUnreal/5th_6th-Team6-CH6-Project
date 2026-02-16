c7604bd5ed893d16ea35b09c2f10911f0b319ca7
김하빈 <runk7gkqls@gmail.com>
2026-02-16 23:28:26 +0900

    chore

--- Source/ProjectER/SkillSystem/GameplayAbilityTargetActor/MouseLocationTargetActor.cpp
+++ Source/ProjectER/SkillSystem/GameplayAbilityTargetActor/MouseLocationTargetActor.cpp
@@ -35,12 +35,12 @@ bool AMouseLocationTargetActor::TryConfirmMouseLocation()
 	FVector MouseLocation = FVector::ZeroVector;
 	if (!MouseClickSkill->TryGetMouseLocationInRange(MouseLocation)) return false;
 
-	FGameplayAbilityTargetDataHandle DataHandle;
-	FGameplayAbilityTargetData_LocationInfo* LocData = new FGameplayAbilityTargetData_LocationInfo();
-	LocData->TargetLocation.LiteralTransform = FTransform(MouseLocation);
-	DataHandle.Add(LocData);
+	//FGameplayAbilityTargetDataHandle DataHandle;
+	//FGameplayAbilityTargetData_LocationInfo* LocData = new FGameplayAbilityTargetData_LocationInfo();
+	//LocData->TargetLocation.LiteralTransform = FTransform(MouseLocation);
+	//DataHandle.Add(LocData);
 
-	TargetDataReadyDelegate.Broadcast(DataHandle);
+	TargetDataReadyDelegate.Broadcast(MakeLocationTargetData(MouseLocation));
 	return true;
 }
 
