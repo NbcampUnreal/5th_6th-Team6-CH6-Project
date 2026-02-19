ec2a6ebe6a2b54229eeba67c196098aa58a8b64d
김하빈 <runk7gkqls@gmail.com>
2026-02-19 09:57:12 +0900

    Add USummonRangeGEC

---
+++ Source/ProjectER/SkillSystem/GameplayEffectComponent/SummonRangeGEC.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SkillSystem/GameplayEffectComponent/BaseGEC.h"
+#include "SkillSystem/GameplayEffectComponent/BaseGECConfig.h"
+#include "SummonRangeGEC.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
+class PROJECTER_API USummonRangeByWorldOriginGECConfig : public UBaseGECConfig
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<AActor> RangeActorClass;
+
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = "true"))
+	float LifeSpan = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = "true"))
+	float ZOffset = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = "true"))
+	FRotator SpawnRotation = FRotator::ZeroRotator;
+};
+
+UCLASS()
+class PROJECTER_API USummonRangeGEC : public UBaseGEC
+{
+	GENERATED_BODY()
+	
+public:
+	USummonRangeGEC();
+
+	virtual TSubclassOf<UBaseGECConfig> GetRequiredConfigClass() const override;
+
+protected:
+	virtual void OnGameplayEffectExecuted(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec, FPredictionKey& PredictionKey) const override;
+};
