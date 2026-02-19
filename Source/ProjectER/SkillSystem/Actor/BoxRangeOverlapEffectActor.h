f4bc75dafd6e0958476d1cc4aa3e3afd690a20d8
김하빈 <runk7gkqls@gmail.com>
2026-02-19 14:20:05 +0900

    Update BaseRangeOverlapEffectActor

---
+++ Source/ProjectER/SkillSystem/Actor/BoxRangeOverlapEffectActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SkillSystem/Actor/BaseRangeOverlapEffectActor.h"
+#include "BoxRangeOverlapEffectActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API ABoxRangeOverlapEffectActor : public ABaseRangeOverlapEffectActor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectER/SkillSystem/Actor/CapsuleRangeOverlapEffectActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SkillSystem/Actor/BaseRangeOverlapEffectActor.h"
+#include "CapsuleRangeOverlapEffectActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API ACapsuleRangeOverlapEffectActor : public ABaseRangeOverlapEffectActor
+{
+	GENERATED_BODY()
+	
+};
