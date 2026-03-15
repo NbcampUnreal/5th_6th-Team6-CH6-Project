8e2b4ead61b81caa03b0073c009cec9692db5992
김하빈 <runk7gkqls@gmail.com>
2026-03-15 22:12:58 +0900

    Update ProjectileBase

--- Source/ProjectER/SkillSystem/Actor/Projectile/ProjectileBase.h
+++ Source/ProjectER/SkillSystem/Actor/Projectile/ProjectileBase.h
@@ -3,11 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "SkillSystem/Actor/BaseRangeOverlapEffectActor.h"
 #include "ProjectileBase.generated.h"
 
 UCLASS()
-class PROJECTER_API AProjectileBase : public AActor
+class PROJECTER_API AProjectileBase : public ABaseRangeOverlapEffectActor
 {
 	GENERATED_BODY()
 	
@@ -15,12 +15,22 @@ public:
 	// Sets default values for this actor's properties
 	AProjectileBase();
 
+	void SetProjectileMovement(float InitialSpeed, float GravityScale, bool bInDestroyOnHit);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	virtual void OnShapeBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	TObjectPtr<class UProjectileMovementComponent> ProjectileMovement;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Projectile")
+	bool bDestroyOnHit = true;
 };
