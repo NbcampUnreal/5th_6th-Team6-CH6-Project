1d532db9539652482ede2c174deec2fc450996ea
김하빈 <runk7gkqls@gmail.com>
2026-03-17 12:03:47 +0900

    Update SummonPeriodicPoolGEC.cpp

--- Source/ProjectER/SkillSystem/GameplayEffectComponent/SummonPeriodicPoolGEC.cpp
+++ Source/ProjectER/SkillSystem/GameplayEffectComponent/SummonPeriodicPoolGEC.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SkillSystem/GameplayEffectComponent/SummonPeriodicPoolGEC.h"
-#include "SkillSystem/Actor/PeriodicOverlapPoolActor/PeriodicOverlapPoolActor.h"
+#include "SkillSystem/GameplayEffectComponent/SummonPeriodicPoolConfig.h"
+#include "SkillSystem/Actor/BaseRangeOverlapEffectActor/BaseRangeOverlapEffectActor.h"
+#include "SkillSystem/Component/AreaPeriodicEffectComponent.h"
 
 TSubclassOf<UBaseGECConfig> USummonPeriodicPoolGEC::GetRequiredConfigClass() const
 {
@@ -13,12 +15,21 @@ void USummonPeriodicPoolGEC::InitializeRangeActor(ABaseRangeOverlapEffectActor* 
     // 부모의 초기화 로직 (Effect Specs 설정 등) 실행
     Super::InitializeRangeActor(RangeActor, Config, Instigator, Context, HitTargetCueParameters);
 
-    // 주기적 효과 데이터 설정
-    if (APeriodicOverlapPoolActor* PeriodicActor = Cast<APeriodicOverlapPoolActor>(RangeActor))
+    if (IsValid(RangeActor) && IsValid(PeriodicConfig))
     {
-        if (const USummonPeriodicPoolConfig* PeriodicConfig = Cast<USummonPeriodicPoolConfig>(Config))
+        // 1. AreaPeriodicEffectComponent 동적 생성
+        UAreaPeriodicEffectComponent* PeriodicComp = NewObject<UAreaPeriodicEffectComponent>(RangeActor, UAreaPeriodicEffectComponent::StaticClass(), TEXT("DynamicAreaPeriodicEffect"));
+        if (IsValid(PeriodicComp))
         {
-            PeriodicActor->InitializePeriodicData(PeriodicConfig->Period, PeriodicConfig->bApplyImmediately);
+            PeriodicComp->CreationMethod = EComponentCreationMethod::Instance;
+
+            // 2. 컴포넌트 등록 및 액터 할당
+            PeriodicComp->RegisterComponent();
+            RangeActor->AddInstanceComponent(PeriodicComp);
+            RangeActor->SetAreaPeriodicComponent(PeriodicComp);
+
+            // 3. 주기적 효과 설정 (실행은 액터의 BeginPlay에서 담당)
+            PeriodicComp->SetupPeriodicEffect(PeriodicConfig->Period, PeriodicConfig->bApplyImmediately);
         }
     }
 }
