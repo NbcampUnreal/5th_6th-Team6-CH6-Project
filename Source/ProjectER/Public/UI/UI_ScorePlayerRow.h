d7868eba8676317e0b56c36e485734281ec2e179
CranberryIsGoodTaste <104151171+CranberryMan@users.noreply.github.com>
2026-03-18 20:29:50 +0900

    Add : ScoreBoard CPP

---
+++ Source/ProjectER/Public/UI/UI_ScorePlayerRow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetBlueprint.h"
+#include "UI_ScorePlayerRow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API UUI_ScorePlayerRow : public UWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectER/Public/UI/UI_Scoreboard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetBlueprint.h"
+#include "UI_Scoreboard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTER_API UUI_Scoreboard : public UWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+};
