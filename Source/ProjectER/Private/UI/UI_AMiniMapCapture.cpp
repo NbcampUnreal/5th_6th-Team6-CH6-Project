8503ef4049b4d12a1579c762451f31f1ca8edea6
CranberryIsGoodTaste <104151171+CranberryMan@users.noreply.github.com>
2026-03-23 20:04:02 +0900

    Add : Scoreboard Minimap

---
+++ Source/ProjectER/Private/UI/UI_AMiniMapCapture.cpp
@@ -0,0 +1,42 @@
+﻿#include "UI/UI_AMiniMapCapture.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+#include "Engine/TextureRenderTarget2D.h"
+
+// Sets default values
+AUI_AMiniMapCapture::AUI_AMiniMapCapture()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    CaptureComponent = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("CaptureComponent"));
+    RootComponent = CaptureComponent;
+
+    CaptureComponent->SetRelativeRotation(FRotator(-90.0f, 0.0f, 0.0f));
+    CaptureComponent->ProjectionType = ECameraProjectionMode::Orthographic;
+
+    // CaptureComponent->CaptureSource = SCS_SceneColorHDR;
+    CaptureComponent->bCaptureEveryFrame = true;
+    CaptureComponent->bCaptureOnMovement = false;
+
+
+
+
+
+    //// 미니맵 캡처 기본 설정
+    //CaptureComponent->SetAbsolute(true, true, true); // 순서대로: 위치, 회전, 스케일
+    //// 위치는 캐릭터를 따라다녀야 함으로 앱솔루트 ㄴㄴ
+    //CaptureComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 1000.0f));
+    //CaptureComponent->SetRelativeRotation(FRotator(-90.0f, 0.0f, 0.0f));
+    CaptureComponent->CaptureSource = ESceneCaptureSource::SCS_FinalColorLDR;	// 투명도 반영
+}
+
+// Called when the game starts or when spawned
+void AUI_AMiniMapCapture::BeginPlay()
+{
+	Super::BeginPlay();
+
+    SetActorLocation(FVector(0.0f, 0.0f, 5000.0f));
+    CaptureComponent->OrthoWidth = 10000.0f; // 맵 전체가 다 들어올 정도의 너비
+	
+}
+
---
+++ Source/ProjectER/Public/UI/UI_AMiniMapCapture.h
@@ -0,0 +1,26 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "UI_AMiniMapCapture.generated.h"
+
+UCLASS()
+class PROJECTER_API AUI_AMiniMapCapture : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+    AUI_AMiniMapCapture();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere)
+    class USceneCaptureComponent2D* CaptureComponent;
+
+    UPROPERTY(EditAnywhere)
+    class UTextureRenderTarget2D* MapRenderTarget;
+
+};
